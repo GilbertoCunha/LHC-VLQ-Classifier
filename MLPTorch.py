edebdf78c1e1fe46a67fb0dfd09f3f04466a4fdf
Gilberto Cunha <gcacademic@outlook.pt>
2020-05-15 22:15:36 +0100

    Add validation to torch

--- MLPTorch.py
+++ MLPTorch.py
@@ -30,7 +30,7 @@ class MLP(nn.Module):
         return t
 
 
-# Dataset MNIST
+# Load MNIST Datasets
 dataset = torchvision.datasets.MNIST(
     root='',
     download=True,
@@ -39,30 +39,73 @@ dataset = torchvision.datasets.MNIST(
     ]),
     train=True
 )
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=64, shuffle=True)
+test_set = torchvision.datasets.MNIST(
+    root='',
+    download=True,
+    transform=transforms.Compose([
+        transforms.ToTensor()
+    ]),
+    train=False
+)
+
+# Generate loaders
+train_size = 0.9*len(dataset)
+splitting = [int(train_size), len(dataset) - int(train_size)]
+train_set, valid_set = torch.utils.data.random_split(dataset, splitting)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=64, shuffle=True)
+valid_loader = torch.utils.data.DataLoader(valid_set, batch_size=64)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=64)
 
 # Parametros de ajuste do modelo
 model = MLP(28*28, 10).to(device)
 optimizer = torch.optim.Adam(model.parameters())
-epochs = 20
+epochs = 10
+best_valid_loss = float("inf")
 
 # Ciclo de treino
 for epoch in range(epochs):
-    train_acc, train_loss = 0, 0
 
-    for image, label in tqdm(dataloader, total=len(dataloader), desc=f"Epoch {epoch+1}"):
-        image, label = image.to(device), label.to(device)
-        prediction = model(image)
-        loss = F.cross_entropy(prediction, label)
+    # Training
+    train_acc, train_loss = 0, 0
+    for images, labels in tqdm(train_loader, total=len(train_loader), desc=f"Epoch {epoch+1} Train"):
+        images, labels = images.to(device), labels.to(device)
+        predictions = model(images)
+        loss = F.cross_entropy(predictions, labels)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        train_acc += prediction.argmax(dim=-1).eq(label).sum().item()
+        train_acc += predictions.argmax(dim=-1).eq(labels).sum().item()
         train_loss += loss.item()
+    train_loss = train_loss / len(train_loader)
+    train_acc = train_acc / len(train_loader.dataset)
 
-    train_loss = train_loss / len(dataloader)
-    train_acc = train_acc / len(dataloader.dataset)
+    # Validation
+    valid_loss, valid_acc = 0, 0
+    for images, labels in tqdm(valid_loader, total=len(valid_loader), desc=f"Epoch {epoch+1} Valid"):
+        images, labels = images.to(device), labels.to(device)
+        predictions = model(images)
 
-    print(f"Epoch {epoch+1} loss: {train_loss} | Epoch {epoch+1} acc: {train_acc}")
+        loss = F.cross_entropy(predictions, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+
+        valid_acc += predictions.argmax(dim=-1).eq(labels).sum().item()
+        valid_loss += loss.item()
+    valid_loss = valid_loss / len(valid_loader)
+    valid_acc = valid_acc / len(valid_loader.dataset)
+
+    # Save model
+    if valid_loss < best_valid_loss:
+        best_valid_ac = valid_acc
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'valid_loss': valid_loss,
+            'valid_acc': valid_acc
+        }, "Models/torch_model.tar")
+
+    tqdm.write(f"Epoch {epoch+1} loss: {train_loss} | Epoch {epoch+1} acc: {train_acc}")
